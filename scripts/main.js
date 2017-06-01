3935770d80c3d2463b8b1a544ba63ef0ca955f87
Maxwell Stephens <Stephensm2011@gmail.com>
2017-05-31 22:34:35 -0400

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/max.jpg');
     }
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to the site, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to the site, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
